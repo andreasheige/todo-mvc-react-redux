eeb33a02c62d0ecfc027243321ce89f5666802d2
Andreas Heige <a.heige.83@gmail.com>
2020-04-22 14:51:59 +0200

    push

---
+++ src/containers/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+
+import * as TodoActions from '../actions';
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+
+const App = ({ todos, actions }) => (
+    <div>
+    <Header addTodo={actions.addTodo} />
+    <MainSection todos={todos} actions={actions} />
+    </div>
+);
+
+    App.propTypes = {
+        todos: PropTypes.array.isRequired,
+    };
+
+    const mapStateToProps = state => ({
+        todos: state.todos,
+    });
+
+    const mapDispatchToProps = dispatch => ({
+        actions: bindActionCreators(TodoActions, dispatch),
+    });
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
