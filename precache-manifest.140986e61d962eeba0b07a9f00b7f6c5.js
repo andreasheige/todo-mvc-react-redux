462ebfdfd26e218391caccfcde144b1b6f68cfb3
Andreas Heige <a.heige.83@gmail.com>
2020-05-16 16:20:16 +0200

    Updates

---
+++ precache-manifest.140986e61d962eeba0b07a9f00b7f6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337156cd938a003ae43d0e5e8debb96f",
+    "url": "/todo-mvc-react-redux/index.html"
+  },
+  {
+    "revision": "37872336b286770f83ee",
+    "url": "/todo-mvc-react-redux/static/css/2.170b32f1.chunk.css"
+  },
+  {
+    "revision": "37872336b286770f83ee",
+    "url": "/todo-mvc-react-redux/static/js/2.3927cd5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-mvc-react-redux/static/js/2.3927cd5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015b6e04a7dd2730123d",
+    "url": "/todo-mvc-react-redux/static/js/main.577ef96a.chunk.js"
+  },
+  {
+    "revision": "1302ffb1425ba61ee640",
+    "url": "/todo-mvc-react-redux/static/js/runtime-main.d4b04ead.js"
+  }
+]);
